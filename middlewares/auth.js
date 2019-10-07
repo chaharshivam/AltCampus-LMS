e22d4fef5299bf9719f68ed40bb7b2dc5854b754
chaharshivam <chahar.shivam@yahoo.com>
2019-10-07 19:36:11 +0530

    Fix: id in request

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,14 +6,11 @@ exports.generateToken = function (payload) {
 
 exports.verifyToken = function(req, res, next) {
     const token = req.headers.authorization || "";
-
+    
     if (token) {
         jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            
             if (err) return res.status(400).json({ error: "Invalid Token"});
-            
-            req.userId = decoded.userId;
-
+            req.userId = decoded.id;
             next();
         });
     } else {
