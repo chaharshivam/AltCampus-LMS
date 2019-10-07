f2062c7e3e927bfd5d774936bef9b6449f936e40
chaharshivam <chahar.shivam@yahoo.com>
2019-10-07 21:22:22 +0530

    Fix: Indent

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 exports.generateToken = function (payload) {
-    return jwt.sign(payload, process.env.JWT_SECRET);
+  return jwt.sign(payload, process.env.JWT_SECRET);
 }
 
-exports.verifyToken = function(req, res, next) {
-    const token = req.headers.authorization || "";
-    
-    if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if (err) return res.status(400).json({ error: "Invalid Token"});
-            req.userId = decoded.id;
-            next();
-        });
-    } else {
-        return res.status(401).json({ error: "Unauthorized access" });
-    }
+exports.verifyToken = function (req, res, next) {
+  const token = req.headers.authorization || "";
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) return res.status(400).json({ error: "Invalid Token" });
+      req.userId = decoded.id;
+      next();
+    });
+  } else {
+    return res.status(401).json({ error: "Unauthorized access" });
+  }
 }
