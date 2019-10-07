9e362ba20f8c0522c3010b5666e5a0542d67f21e
chaharshivam <chahar.shivam@yahoo.com>
2019-10-07 21:21:40 +0530

    Add: Assignment routes

--- routes/api/assignments.js
+++ routes/api/assignments.js
@@ -1,5 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const assignment = require('../../controllers/assignment');
+const auth = require('../../middlewares/auth');
 
-
+router
+  .route('/')
+  .get(auth.verifyToken, assignment.all)
+  .post(auth.verifyToken, assignment.create);
+  
+router
+  .route('/:id')  
+  .put(auth.verifyToken, assignment.update)
+  .delete(auth.verifyToken, assignment.delete);
+  
 module.exports = router;
