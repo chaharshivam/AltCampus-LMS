e3f0f5689f0838e0947243c6e19c6c3e42c40067
chaharshivam <chahar.shivam@yahoo.com>
2019-10-08 19:44:02 +0530

    Update: projects routes

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,5 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const project = require('../../controllers/project');
+const auth = require('../../middlewares/auth');
 
+router
+  .route('/')
+  .get(project.all)
+  .post(auth.verifyToken, project.create)
+
+router
+  .route('/:id')  
+  .put(auth.verifyToken, project.update)
+  .delete(auth.verifyToken, project.delete)
 
 module.exports = router;
