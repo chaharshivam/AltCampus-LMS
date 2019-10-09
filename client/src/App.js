4dbb4c8f4f723657b02699232bfc424438ce9022
Swastikyadav <swastikyadav.dev@gmail.com>
2019-10-10 02:52:27 +0530

    sidebar and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="flex-start">
       <Sidebar />
       <aside className="main-content">
-        
+        <Header />
       </aside>
     </div>
   );
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -18,80 +18,83 @@ class Sidebar extends React.Component {
 
   render() {
     return (
-      <aside className="sidebar">
-        <div className="sidebar-wrapper">
-          <div className="logo-div">
-            <a href="#">
-              <img src="/lms1.png" />
-            </a>
+      <div className="sidebar-holder">
+        <input type="checkbox" className="sidebar-checkbox" id="checkbox_toggle" />
+        <aside className="sidebar">
+          <div className="sidebar-wrapper">
+            <div className="logo-div">
+              <a href="#">
+                <img src="/lms1.png" />
+              </a>
+            </div>
+            <span onClick={this.handleClick}>
+              <a
+                href="#"
+                className={
+                  this.state.active == `DASHBOARD`
+                    ? "flex-start active"
+                    : "flex-start"
+                }
+              >
+                <i className="fa fa-home icon"></i>
+                DASHBOARD
+              </a>
+            </span>
+            <span onClick={this.handleClick}>
+              <a
+                href="#"
+                className={
+                  this.state.active == `PROFILE`
+                    ? "flex-start active"
+                    : "flex-start"
+                }
+              >
+                <i className="fa fa-user icon"></i>
+                PROFILE
+              </a>
+            </span>
+            <span onClick={this.handleClick}>
+              <a
+                href="#"
+                className={
+                  this.state.active == `DAILY NOTES`
+                    ? "flex-start active"
+                    : "flex-start"
+                }
+              >
+                <i className="fa fa-book icon"></i>
+                DAILY NOTES
+              </a>
+            </span>
+            <span onClick={this.handleClick}>
+              <a
+                href="#"
+                className={
+                  this.state.active == `ASSIGNMENTS`
+                    ? "flex-start active"
+                    : "flex-start"
+                }
+              >
+                <i className="fa fa-pencil icon"></i>
+                ASSIGNMENTS
+              </a>
+            </span>
+            <span style={{ margin: "160px 0" }} onClick={this.handleClick}>
+              <a
+                href="#"
+                className={
+                  this.state.active == `ASSIGNMENTS`
+                    ? "flex-start active"
+                    : "flex-start"
+                }
+              >
+                <i className="fa fa-users icon"></i>
+                Meet The Creators
+              </a>
+            </span>
           </div>
-          <span onClick={this.handleClick}>
-            <a
-              href="#"
-              className={
-                this.state.active == `DASHBOARD`
-                  ? "flex-start active"
-                  : "flex-start"
-              }
-            >
-              <i className="fa fa-home icon"></i>
-              DASHBOARD
-            </a>
-          </span>
-          <span onClick={this.handleClick}>
-            <a
-              href="#"
-              className={
-                this.state.active == `PROFILE`
-                  ? "flex-start active"
-                  : "flex-start"
-              }
-            >
-              <i className="fa fa-user icon"></i>
-              PROFILE
-            </a>
-          </span>
-          <span onClick={this.handleClick}>
-            <a
-              href="#"
-              className={
-                this.state.active == `DAILY NOTES`
-                  ? "flex-start active"
-                  : "flex-start"
-              }
-            >
-              <i className="fa fa-book icon"></i>
-              DAILY NOTES
-            </a>
-          </span>
-          <span onClick={this.handleClick}>
-            <a
-              href="#"
-              className={
-                this.state.active == `ASSIGNMENTS`
-                  ? "flex-start active"
-                  : "flex-start"
-              }
-            >
-              <i className="fa fa-pencil icon"></i>
-              ASSIGNMENTS
-            </a>
-          </span>
-          <span style={{margin: '160px 0'}} onClick={this.handleClick}>
-            <a
-              href="#"
-              className={
-                this.state.active == `ASSIGNMENTS`
-                  ? "flex-start active"
-                  : "flex-start"
-              }
-            >
-              <i className="fa fa-users icon"></i>
-              Meet The Creators
-            </a>
-          </span>
-        </div>
-      </aside>
+        </aside>
+      </div>
     );
   }
 }
