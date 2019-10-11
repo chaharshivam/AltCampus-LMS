1dd0b3d2b732e8444ce9629221a079e3746fc9c4
chaharshivam <chahar.shivam@yahoo.com>
2019-10-12 03:15:26 +0530

    Update: profile

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,19 +1,48 @@
 import React from 'react';
+import { withRouter, Switch, Route } from 'react-router-dom';
+
 import ProfileCard from '../../components/ProfileCard/ProfileCard';
 import Members from '../../components/Members/Members';
 import Projects from '../../components/Projects/Projects';
+import EditProfile from '../../components/ProfileSettings/EditProfile';
+import EditProjects from '../../components/ProfileSettings/EditProjects';
+import EditBlogs from '../../components/ProfileSettings/EditBlogs';
+import BlogCard from '../../components/Blogs/BlogCard';
 
-function Profile(props) {
+function Profile({ match }) {
   return (
     <main>
       <div className="wrapper">
-        <div className="row">
+        <div className="row flex-between">
           <div className="col">
             <ProfileCard />
             <Members />
           </div>
-          <div className="projects">
-            <Projects />
+          <div className="aside flex-between">
+            <Switch>
+              <Route path="/profile/settings">
+                <React.Fragment>
+                  <EditProfile />
+                  <EditProjects />
+                  <EditBlogs />
+                </React.Fragment>
+              </Route>
+              <Route path="/profile/story">
+                <h1>Story</h1>
+              </Route>
+              <Route path="/profile/blogs">
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+              </Route>
+              <Route path="/">
+                <Projects />
+                <Projects />
+                <Projects />
+                <Projects />
+              </Route>
+            </Switch>
           </div>
         </div>
       </div>
@@ -21,4 +50,4 @@ function Profile(props) {
   );
 }
 
-export default Profile;
+export default withRouter(Profile);
