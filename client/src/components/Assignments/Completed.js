ce4b6431140ecdb7950715cc3a50e02f56e5f1c1
Swastikyadav <swastikyadav.dev@gmail.com>
2019-10-12 15:25:44 +0530

    API Integration Assignments

--- client/src/components/Assignments/Completed.js
+++ client/src/components/Assignments/Completed.js
@@ -1,22 +1,30 @@
 import React from "react";
+import userContext from "../../context/userContext";
 
 function Completed() {
+  const { user } = React.useContext(userContext);
+  console.log(user);
   return (
     <div className="assignment-card wrapper">
-      <h5 style={{color: 'lightgreen'}}>Completed Assignments</h5>
+      <h5 style={{ color: "lightgreen" }}>Completed Assignments</h5>
       <ul>
-        <li>
-          <a href="#">Async JS</a>
-        </li>
-        <li>
-          <a href="#">OOP JS</a>
-        </li>
-        <li>
-          <a href="#">Clousers JS</a>
-        </li>
-        <li>
-          <a href="#">Methods JS</a>
-        </li>
+        {user ? (
+          user.completed_assignments.length ? (
+            user.completed_assignments.map(assi => {
+              return (
+                <>
+                  <li>
+                    <a href={assi.repo_url}>{assi.title}</a>
+                  </li>
+                </>
+              );
+            })
+          ) : (
+            <p>No assignments here...</p>
+          )
+        ) : (
+          console.log("No user")
+        )}
       </ul>
     </div>
   );
--- client/src/components/Assignments/Pending.js
+++ client/src/components/Assignments/Pending.js
@@ -1,30 +1,31 @@
 import React from "react";
+import UserContext from "../../context/userContext";
 
 function Pending() {
+  const { user } = React.useContext(UserContext);
   return (
     <div className="assignment-card wrapper">
-      <h5 style={{color: "red"}}>Pending Assignments</h5>
+      <h5 style={{ color: "red" }}>Pending Assignments</h5>
       <ul>
-        <li>
-          <a href="#">Async JS</a>
-          <input type="text" placeholder="Pull Request Url" />
-          <input type="submit" value="Submit" />
-        </li>
-        <li>
-          <a href="#">OOP JS</a>
-          <input type="text" placeholder="Pull Request Url" />
-          <input type="submit" value="Submit" />
-        </li>
-        <li>
-          <a href="#">Clousers JS</a>
-          <input type="text" placeholder="Pull Request Url" />
-          <input type="submit" value="Submit" />
-        </li>
-        <li>
-          <a href="#">Methods JS</a>
-          <input type="text" placeholder="Pull Request Url" />
-          <input type="submit" value="Submit" />
-        </li>
+        {user ? (
+          user.assignments.length ? (
+            user.assignments.map(assi => {
+              return (
+                <>
+                  <li>
+                    <a href={assi.repo_url}>{assi.title}</a>
+                    <input type="text" placeholder="Pull Request Url" />
+                    <input type="submit" value="Submit" />
+                  </li>
+                </>
+              );
+            })
+          ) : (
+            <p>No assignments here...</p>
+          )
+        ) : (
+          console.log("No user")
+        )}
       </ul>
     </div>
   );
