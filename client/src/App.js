ac86bfa31c23d0e9c6d123bb2b939ac6f605c27f
chaharshivam <chahar.shivam@yahoo.com>
2019-10-13 11:56:52 +0530

    Fix: OAuth in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 
 import UserContext, { UserProvider } from "./context/userContext";
 import API from "./utils/API";
@@ -35,6 +35,14 @@ class App extends React.Component {
     }
   };
 
+  oAuth = (token) => {
+    if(token == 'undefined' || !token) return;
+
+    localStorage.setItem("token", token);
+    this.updateUser();
+    this.props.history.push('/home');
+  }
+
   updateHeader = active => {
     this.setState({ active });
   };
@@ -46,6 +54,9 @@ class App extends React.Component {
   publicRoutes = () => {
     return (
       <Switch>
+        <Route path='/oauth'>
+          { this.oAuth(this.props.location.search.split('=')[1]) }
+        </Route>
         <Route path="/login">
           <UserProvider
             value={{ user: this.state.user, updateUser: this.updateUser }}
@@ -53,7 +64,6 @@ class App extends React.Component {
             <Login />
           </UserProvider>
         </Route>
-
         <Route>
           <h2>Page not found</h2>
         </Route>
@@ -114,4 +124,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withRouter(App);
